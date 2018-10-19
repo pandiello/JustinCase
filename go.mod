452b5516bf550442931b68fd2f1b40901445e9ab
Diego Fernández <d.fernandez@payvision.com>
2018-10-19 14:44:23 +0200

    include local sql insert as example. Hardcode credentials for local database

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/pandiello/JustinCase
+
+require (
+	cloud.google.com/go v0.30.0 // indirect
+	github.com/denisenkom/go-mssqldb v0.0.0-20181014144952-4e0d7dc8888f
+)
--- main.go
+++ main.go
@@ -1,41 +1,56 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/pandiello/JustinCase/model"
 )
 
 func main() {
 
-	records, err := ReadFile("caca.csv")
+	records, err := readFile("fake.csv")
 	fatalIf(err)
 
 	headerOffset := 1
 	var genericTransactions []model.GenericTransaction
 	for _, row := range records[headerOffset:] {
-
-		genericTransactions = append(genericTransactions, model.GenericTransaction{
-			ID:          row[0],
-			Name:        row[1],
-			Description: row[2],
-		})
+		genericTransactions = append(genericTransactions, mapRow(row))
 	}
 
-	fmt.Printf("%+v\n", genericTransactions)
+	connectionString := "sqlserver://JustinCase:Perro@localhost"
+	db, err := sql.Open("sqlserver", connectionString)
+	for _, gt := range genericTransactions {
+		_, err := db.Exec(`INSERT INTO GenericTransaction (ID,Name,Description,Date)
+		values (@p1,@p2,@p3,@p4)`, gt.ID, gt.Name, gt.Description, time.Now())
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
 }
 
 // ReadFile reads a csv file using the given file path.
-func ReadFile(fileName string) ([][]string, error) {
+func readFile(fileName string) ([][]string, error) {
 	file, err := os.Open(fileName) // For read access.
 	if err != nil {
 		return nil, err
 	}
 	r := csv.NewReader(file)
 	return r.ReadAll()
+}
+
+func mapRow(row []string) model.GenericTransaction {
+	return model.GenericTransaction{
+		ID:          row[0],
+		Name:        row[1],
+		Description: row[2],
+	}
 }
 
 func fatalIf(err error) {
